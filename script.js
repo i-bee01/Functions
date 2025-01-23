ed333be616c022ba975de25d71257a2eb157060f
Iberedem <ibee2k12@gmail.com>
2025-01-23 15:51:15 -0700

    Work in Progress

---
+++ script.js
@@ -0,0 +1,28 @@
+//1.)
+function avg(num1, num2, num3){
+    let i=3;
+    let average = (num1+num2+num3)/i;
+    return average;
+}
+
+console.log(avg(4,8,12));
+
+//2.)
+let testNum1 = 8;
+let testNum2 = 10;
+let testNum3 = 3;
+
+let computedAverage = avg(testNum1,testNum2,testNum3);
+
+console.log(computedAverage);
+
+//3.)
+function isEqual(string1,string2){
+    if  (string1 == string2){
+    return true;
+}
+    else {
+        return false;
+    }
+}
+console.log(isEqual("Ibee", "Ibee"));
